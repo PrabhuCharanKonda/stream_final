ec1f62e94524edff769a663376d426cc20297f25
PrabhuCharanKonda <prabhucharan.konda@gmail.com>
2025-08-22 01:41:08 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://stream-final-back.onrender.com";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://stream-final-back.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
